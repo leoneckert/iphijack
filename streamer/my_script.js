0c307b489812f6d2d11a59bf3b736cb8b5448de9
Leon Eckert <eckertleon@gmail.com>
2016-12-06 03:10:14 -0500

    local server shift

--- streamer/my_script.js
+++ streamer/my_script.js
@@ -67,7 +67,8 @@ function allocateElement(idx, callback){
         button.innerHTML = "encode a message";
         elem.appendChild(button);
         button.addEventListener ("click", function() {
-            alert(document.getElementById("text_"+idx_id.value);
+
+            alert(document.getElementById("text_"+idx_id).value);
             var xhttp = new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
                 if (this.readyState == 4 && this.status == 200) {
