d81dd5fb7dc0345252a9dec97557df7ee95818fe
Leon Eckert <eckertleon@gmail.com>
2016-12-05 21:42:04 -0500

    local server shift

--- streamer/index.js
+++ streamer/index.js
@@ -272,10 +272,10 @@ server.get('/loadingImg', function(req, res){
 
 server.get('/encodeReq', function(req, res){
     console.log("req");
-    console.log(req.query);
+    console.log(Object.keys(req.query)[0]);
 
-    console.log(getPixelXY(parseInt(req.query)).x );
-    console.log(getPixelXY(parseInt(req.query)).y );
+    console.log(getPixelXY(176, parseInt(req.query)).x );
+    console.log(getPixelXY(176, parseInt(req.query)).y );
     console.log("|here is data" + " |");
     console.log("|"+ABC.toBinary("here is data" + " ") + " |");
     console.log("idx=0");
